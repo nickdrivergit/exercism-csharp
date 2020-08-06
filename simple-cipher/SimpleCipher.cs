18ecea866828b9b9d07e791a0d539cebb30d0100
nick <nicholasdriver@protonmail.com>
2020-08-06 16:12:56 +0200

    Simple Cipher solution

--- simple-cipher/SimpleCipher.cs
+++ simple-cipher/SimpleCipher.cs
@@ -4,29 +4,65 @@ public class SimpleCipher
 {
     public SimpleCipher()
     {
-        throw new NotImplementedException("You need to implement this function.");
+        string key = "";
+        Random rand = new Random();
+        for(int i = 0;i<100;++i)
+            key += (char)rand.Next('a', 'z');
+        Key = key;
     }
 
     public SimpleCipher(string key)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        Key = key;
     }
     
-    public string Key 
-    {
-        get
-        {
-            throw new NotImplementedException("You need to implement this function.");
-        }
-    }
+    public string Key { get; }
 
     public string Encode(string plaintext)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        string output = "";
+        int cipherIndex = 0;
+        int textIndex = 0;
+        foreach (char c in plaintext){
+            output += EncodeRotate(plaintext[textIndex], Key[cipherIndex]);
+            textIndex++;
+            cipherIndex++;
+            if (cipherIndex == Key.Length)
+                cipherIndex = 0;
+        }
+        return output;
     }
 
     public string Decode(string ciphertext)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        string output = "";
+        int cipherIndex = 0;
+        int textIndex = 0;
+        foreach (char c in ciphertext){
+            output += DecodeRotate(ciphertext[textIndex], Key[cipherIndex]);
+            textIndex++;
+            cipherIndex++;
+            if (cipherIndex == Key.Length)
+                cipherIndex = 0;
+        }
+        return output;
+    }
+
+    public static char EncodeRotate(char c, char n)
+    {
+        int rotate = (int)n - 'a';
+        int cAscii = (int)c;
+        cAscii = 'a' + (((cAscii - 'a') + rotate) % 26);
+        return (char)cAscii;
+    }
+
+    public static char DecodeRotate(char c, char n)
+    {
+        int rotate = (int)n - 'a';
+        int cAscii = (int)c;
+        cAscii = 'a' + (((cAscii - 'a') - rotate) % 26);
+        if (cAscii < 'a')
+            cAscii += 26;
+        return (char)cAscii;
     }
 }
